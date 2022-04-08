2ee5e5c4b9ff2231d7c5b8737400b800be163010
Jason Chan <jasonchan19981017@hotmail.com>
2022-04-08 22:41:35 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -2,12 +2,25 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const app = express();
 const restaurantList = require('./restaurant.json').results;
+const mongoose = require('mongoose');
 const port = 3000;
+require('dotenv').config();
 
+// set up view engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
+// set up mongoDB
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+
+db.on('error', () => console.log('mongoDB error.'));
+
+db.once('open', () => console.log('mongoDB connected.'));
+
+// middleware
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
 	res.render('index', { restaurants: restaurantList });
@@ -20,6 +33,14 @@ app.get('/search', (req, res) => {
 		return restaurant.name.match(regExp) || restaurant.category.match(regExp);
 	});
 	res.render('index', { restaurants: restaurants, keyword: keyword });
+});
+
+app.get('/restaurants/new', (req, res) => {
+	res.render('new');
+});
+
+app.post('/restaurants/new', (req, res) => {
+	console.log(req.body);
 });
 
 app.get('/restaurants/:id', (req, res) => {
