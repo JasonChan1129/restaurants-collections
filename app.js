57d2386cb78a14e2bc5f0cd1d1ed391003c60958
Jason Chan <jasonchan19981017@hotmail.com>
2022-04-14 14:26:08 +0800

    refactor: modify express google api request routes

--- app.js
+++ app.js
@@ -5,9 +5,6 @@ const methodOverride = require('method-override');
 const routes = require('./routes');
 // connect to mongoDB
 require('./config/mongoose');
-require('dotenv').config();
-// import Google Maps Client
-const { Client } = require('@googlemaps/google-maps-services-js');
 
 const port = 3000;
 
@@ -23,38 +20,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(routes);
 
-const client = new Client({});
-// handle auto-fill restaurant details when adding a new restaurant
-app.post('/restaurants/request', async (req, res) => {
-	const input = req.body.input;
-	try {
-		// to get a specific place_id from Google Places API
-		const findPlaceRes = await client.findPlaceFromText({
-			params: {
-				input: input,
-				inputtype: 'textquery',
-				language: 'zh-TW',
-				key: process.env.GOOGLE_API_KEY,
-			},
-		});
-		const placeID = findPlaceRes.data.candidates[0].place_id;
-		// to get place details from Google Places API
-		const placeDetailsRes = await client.placeDetails({
-			params: {
-				place_id: placeID,
-				language: 'zh-TW',
-				key: process.env.GOOGLE_API_KEY,
-			},
-		});
-		const placeDetailsData = placeDetailsRes.data.result;
-		// pass the obtained place details to the 'new' templete
-		return res.render('new', { placeDetailsData });
-	} catch (err) {
-		console.log(err);
-		return res.render('new');
-	}
-});
-
 app.listen(port, () => {
-	console.log(`Server is listening to localhost: ${port}`);
+	console.log(`Server is listening to localhost:${port}`);
 });
--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const home = require('./modules/home');
 const restaurants = require('./modules/restaurants');
+const apiRequest = require('./modules/apiRequest');
 
 router.use('/', home);
 router.use('/restaurants', restaurants);
+router.use('/request', apiRequest);
 
 module.exports = router;
